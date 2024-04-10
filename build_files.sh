cc543c3d9bd50b6a66cee7e62885cd50e979f88a
Programming With Karam <68340649+karam123-bot@users.noreply.github.com>
2024-04-10 12:07:24 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,4 @@
-echo "======> INSTALLING REQUIREMENTS <======"
-pip install -r requirements.txt
-echo "======> REQUIREMENTS INSTALLED <======"
-
-echo "======> COLLECTING STATIC FILES <======"
-python3.9 manage.py collectstatic --noinput --clear
-echo "======> STATIC FILES COLLECTED <======"
-
-echo "======> MAKE-MIGRATIONS <======"
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-echo "======> MAKE-MIGRATIONS-END <======"
+echo "BUILD START"
+ python3.9 -m pip install -r requirements.txt
+ python3.9 manage.py collectstatic --noinput --clear
+ echo "BUILD END"
