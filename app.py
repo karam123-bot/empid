8878bd60d38a048dedbdde7e2f3da2aae4680b87
KARAM <karammohd00@gmail.com>
2024-04-16 12:40:04 +0530

    commit

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import render
-import importlib
 
 def home(request):
     return HttpResponse("Hello, World!")  # A simple view that returns a plain text response
@@ -12,6 +11,8 @@ urlpatterns = [
     path('', home, name='home'),  # Map the home view to the root URL
 ]
 
-# Serving static files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# Serving static files during development and in production if DEBUG is True
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Serving media files in development and production if DEBUG is True
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
